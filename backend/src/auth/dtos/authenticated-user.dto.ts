2ce546a1d14ab7eadd455a65a604a2ad077710bd
Mateusz_Borowiecki <borowieckimateusz1@gmail.com>
2024-01-26 21:38:20 +0100

    Feature/information (#11)
    * feature: db informations
    
    * feature: readme
    
    * feature(backend): swagger documentation

--- backend/src/auth/dtos/authenticated-user.dto.ts
+++ backend/src/auth/dtos/authenticated-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AuthenticatedUserDto {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   token: string;
 }
